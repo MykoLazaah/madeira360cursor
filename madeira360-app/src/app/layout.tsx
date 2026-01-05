3befbc96e27e257275791f6e2f230a66ee13c08f
Mykola <mykola.zagoruyko@gmail.com>
2026-01-05 20:54:42 +0100

    Add Google Tag Manager (GTM-KMZTJCS4)

--- madeira360-app/src/app/layout.tsx
+++ madeira360-app/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { headers } from 'next/headers'
+import Script from 'next/script'
 import { Open_Sans, Fraunces } from 'next/font/google'
 import './globals.css'
 
@@ -37,6 +38,29 @@ export default function RootLayout({
       <body
         className={`${openSans.variable} ${fraunces.variable} antialiased`}
       >
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KMZTJCS4');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KMZTJCS4"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {/* GetYourGuide Analytics */}
         <script
           async
